f2701eff1b793ae1581854ba9cf8cd07f562716a
Firuza Aliyeva <firuza.aliyeva@procure.az>
2022-12-27 15:04:16 +0400

    Properties list ready

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,32 +1,31 @@
 package com.example.compose
 
 import android.os.Bundle
-import android.widget.Space
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose.ui.theme.ComposeTheme
 import com.example.compose.ui.theme.DarkBlue
-import com.example.compose.ui.theme.Purple200
 import com.example.compose.ui.theme.Teal200
+import com.example.compose.ui.theme.TextFieldBG
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -107,39 +106,40 @@ fun Properties() {
 
         Column(modifier = Modifier.padding(10.dp, 25.dp)) {
 
-            Text(
-                text = "Name",
-                modifier = Modifier.padding(15.dp),
-            )
-            Text(
-                text = "Surname",
-                modifier = Modifier.padding(15.dp),
-            )
-            Text(
-                text = "Phone",
-                modifier = Modifier.padding(15.dp),
-            )
-            Text(
-                text = "Email",
-                modifier = Modifier.padding(15.dp),
-            )
-            Text(
-                text = "Gender",
-                modifier = Modifier.padding(15.dp),
-            )
-            Text(
-                text = "Customer #",
-                modifier = Modifier.padding(15.dp),
-            )
+            Property("Name")
+            Property("Surname")
+            Property("Phone")
+            Property("Email")
+            Property("Gender")
+            Property("Customer #")
+
         }
     }
 
 }
 
 @Composable
+fun Property(name: String) {
+    Column(
+        modifier = Modifier
+            .padding(15.dp, 5.dp)
+            .fillMaxWidth()
+    ) {
+        Text(text = name, color = DarkBlue, fontSize = 15.sp)
+        BasicTextField(
+            value = name,
+            onValueChange = { v -> println(v) },
+            Modifier.fillMaxWidth().padding(0.dp, 4.dp),
+            textStyle = TextStyle(fontSize = 22.sp))
+    }
+}
+
+@Composable
 fun SaveButton() {
     Row(
-        modifier = Modifier.fillMaxSize().background(Color.White),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Bottom
     ) {
@@ -147,7 +147,9 @@ fun SaveButton() {
         Button(
             onClick = {
                 Toast.makeText(context, "Save profile info", Toast.LENGTH_SHORT).show()
-            },modifier = Modifier.padding(0.dp).padding(20.dp)
+            }, modifier = Modifier
+                .padding(0.dp)
+                .padding(20.dp)
         ) {
             Text(text = "Save")
         }
--- app/src/main/java/com/example/compose/Message.kt
+++
@@ -1,5 +0,0 @@
-package com.example.compose
-
-data class ChatMessage (val author:Author, val body:String)
-
-data class Author(val name: String, val imageRes: Int)
