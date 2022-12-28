22638b1ed57280b35f9932653215344c07deaa7b
Firuza Aliyeva <firuza.aliyeva@procure.az>
2022-12-28 12:39:25 +0400

    added avatar gradient

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -15,17 +15,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontSynthesis
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose.ui.theme.ComposeTheme
 import com.example.compose.ui.theme.DarkBlue
-import com.example.compose.ui.theme.Teal200
-import com.example.compose.ui.theme.TextFieldBG
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,70 +51,91 @@ class MainActivity : ComponentActivity() {
 @Preview(name = "Light Mode")
 @Composable
 fun DefaultPreview() {
+    val mockUser = UserProfile(
+        "Firuza", "Aliyeva",
+        "+994503490158", "fsaliyeva96@gmail.com",
+        "female", "meow", R.drawable.firuza
+    )
     ComposeTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            Parent()
+            Parent(mockUser)
 
         }
     }
 }
 
 @Composable
-fun Parent() {
+fun Parent(user: UserProfile) {
     Column(Modifier.background(DarkBlue)) {
-        Header()
-        Properties()
+        Header(user.avatarRes)
+        Properties(user)
         SaveButton()
     }
 }
 
 @Composable
-fun Header() {
+fun Header(imgRes: Int) {
     Card(
         Modifier
             .height(150.dp),
         backgroundColor = Color.Transparent,
         elevation = 0.dp
     ) {
-        Row(
+        Box(
             modifier = Modifier
                 .fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            contentAlignment = Alignment.Center
         ) {
+
             Image(
-                painter = painterResource(id = R.drawable.firuza),
+                painter = painterResource(id = imgRes),
                 contentDescription = "Profile image",
                 Modifier
                     .clip(CircleShape)
                     .height(90.dp)
             )
+            Box(
+                modifier = Modifier
+                    .height(90.dp)
+                    .width(90.dp)
+                    .clip(CircleShape)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ), 160f
+                        )
+                    )
+            ) {
+
+            }
         }
     }
 }
 
 @Composable
-fun Properties() {
+fun Properties(user: UserProfile) {
 
     Card(
         modifier = Modifier
             .fillMaxWidth(),
-        shape = RoundedCornerShape(40.dp, 40.dp, 0.dp, 0.dp),
+        shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp),
         backgroundColor = Color.White,
         elevation = 0.dp
     ) {
 
         Column(modifier = Modifier.padding(10.dp, 25.dp)) {
 
-            Property("Name")
-            Property("Surname")
-            Property("Phone")
-            Property("Email")
-            Property("Gender")
-            Property("Customer #")
+            Property("Name", user.name)
+            Property("Surname", user.Surname)
+            Property("Phone", user.phone)
+            Property("Email", user.email)
+            Property("Gender", user.gender)
+            Property("Customer #", user.customerNo)
 
         }
     }
@@ -119,18 +143,21 @@ fun Properties() {
 }
 
 @Composable
-fun Property(name: String) {
+fun Property(name: String, value: String) {
     Column(
         modifier = Modifier
-            .padding(15.dp, 5.dp)
+            .padding(15.dp, 7.dp)
             .fillMaxWidth()
     ) {
-        Text(text = name, color = DarkBlue, fontSize = 15.sp)
+        Text(text = name, color = DarkBlue, fontSize = 16.sp, fontWeight = FontWeight.Bold)
         BasicTextField(
-            value = name,
+            value = value,
             onValueChange = { v -> println(v) },
-            Modifier.fillMaxWidth().padding(0.dp, 4.dp),
-            textStyle = TextStyle(fontSize = 22.sp))
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 2.dp, bottom = 4.dp),
+            textStyle = TextStyle(fontSize = 19.sp)
+        )
     }
 }
 
@@ -155,3 +182,14 @@ fun SaveButton() {
         }
     }
 }
+
+
+data class UserProfile(
+    val name: String,
+    val Surname: String,
+    val phone: String,
+    val email: String,
+    val gender: String,
+    val customerNo: String,
+    val avatarRes: Int
+)
